f519a56f122475efe93b632206ad55d742f75ed9
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-01-30 14:31:05 +0000

    [EXD-002] - Add Views

---
+++ app/src/Views/home/index.php
@@ -0,0 +1,8 @@
+<!-- TODO: implement default header and footer -->
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2 text-center" style="margin-top:150px">
+            <h1>Home</h1>
+        </div>
+    </div>
+</div>
---
+++ app/src/Views/pagenotfound/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <img src="/assets/img/error404.jpg">
+        </div>
+    </div>
+</div>
