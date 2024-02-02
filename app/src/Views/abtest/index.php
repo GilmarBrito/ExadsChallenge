0ab1d1d5e7baf05c899d92e1f2d0bb0d768fbf56
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-02-02 17:27:20 +0000

    [EXD-019] - Add abtest view

---
+++ app/src/Views/abtest/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__DIR__) . '/header.php';
+?>
+
+<!-- TODO: implement default header and footer -->
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2 text-center" style="margin-top:150px">
+            <h1>Promotion: <?= $data['promotion'] ?></h1>
+            <h2>Design: <?= $data['design'] ?><h2>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once dirname(__DIR__) . '/header.php';
+?>
