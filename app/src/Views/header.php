1abf8a72f6d8384272559d918337a0fbc455046a
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-02-02 17:19:18 +0000

    [EXD-018] - Create basic view snippets

---
+++ app/src/Views/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
