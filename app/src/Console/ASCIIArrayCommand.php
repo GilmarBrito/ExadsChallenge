feb751c1ebd1f0fc08994be48ac8c6e8ddadc07c
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-02-02 16:01:37 +0000

    [EXD-008] - Add ASCIIArray

---
+++ app/src/Console/ASCIIArrayCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ExadsChallenge\Console;
+
+use ExadsChallenge\Libraries\ASCIIArray;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ASCIIArrayCommand extends Command
+{
+    protected function configure(): void
+    {
+        parent::configure();
+        $this
+            ->setName('app:ascii-array')
+            ->setHidden(false)
+            ->setDescription('Execute Prime challenge')
+            ->setHelp('This command show a list...')
+            ->addArgument('startChar', InputArgument::OPTIONAL, 'First number.', ',')
+            ->addArgument('lastChar', InputArgument::OPTIONAL, 'Last number.', '|');
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        //TODO: Take care about excepetions
+        $startChar = $input->getArgument('startChar');
+        $lastChar = $input->getArgument('lastChar');
+
+        $asciiArray = new ASCIIArray($startChar, $lastChar);
+
+        $answer = $asciiArray->getAnswer();
+
+        foreach ($answer as $line) {
+            $output->writeln($line);
+        }
+
+        return Command::SUCCESS;
+    }
+}
